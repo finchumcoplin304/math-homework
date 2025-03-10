3dad40523e74fda88dd48a0579e209787bff70c8
finchumcoplin304 <finchumcoplin304@gmail.com>
2025-03-10 05:09:16 +0000

    Add 5.php

---
+++ src/5.php
@@ -0,0 +1,8 @@
+// Generate a random number between 1 and 10
+$randomNumber = rand(1, 10);
+
+// Use the random number to calculate a square root
+$squareRoot = sqrt($randomNumber);
+
+// Print the result
+echo "The square root of $randomNumber is $squareRoot.";
