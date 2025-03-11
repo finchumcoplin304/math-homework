e9e83aeb86d67e9cf4cd2a3f994fadd8bd6bc509
finchumcoplin304 <finchumcoplin304@gmail.com>
2025-03-11 15:43:53 +0000

    Add 6.php

---
+++ src/6.php
@@ -0,0 +1,11 @@
+ <?php
+
+$number = rand(1, 10); //generate random number between 1 and 10
+
+if ($number % 2 == 0) {
+    echo "The number is even";
+} else {
+    echo "The number is odd";
+}
+
+?>
