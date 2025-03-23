cb8fb5eeb72376b9e3bbbc85c3f900300401a3f4
finchumcoplin304 <finchumcoplin304@gmail.com>
2025-03-23 01:29:46 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,12 @@
+<?php
+// Example PHP code for Math homework assignment 1.6
+
+function calculateArea($width, $height) {
+    return $width * $height;
+}
+
+$width = 5;
+$height = 3;
+
+echo "The area of the rectangle with width " . $width . " and height " . $height . " is: " . calculateArea($width, $height);
+?>
