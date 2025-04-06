256be86f577231bd03bc02d8c6157b62b793f633
finchumcoplin304 <finchumcoplin304@gmail.com>
2025-04-06 06:43:20 +0000

    Add 26.php

---
+++ src/26.php
@@ -0,0 +1,10 @@
+<?php
+function my_function($param1, $param2) {
+    // Add your code here
+}
+
+$first_param = "Hello, this is the first param.";
+$second_param = "World!";
+$result = my_function($first_param, $second_param);
+echo $result;
+?>
