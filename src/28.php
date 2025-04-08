ee8d33184073865890da971babf907f77a33acb8
finchumcoplin304 <finchumcoplin304@gmail.com>
2025-04-08 17:28:43 +0000

    Add 28.php

---
+++ src/28.php
@@ -0,0 +1,24 @@
+<?php
+// Example PHP code to demonstrate various mathematical operations and string manipulations
+
+// Addition of two numbers
+echo "2 + 3 = " . (2 + 3) . "\n";
+
+// Subtraction of two numbers
+echo "5 - 2 = " . (5 - 2) . "\n";
+
+// Multiplication of two numbers
+echo "4 * 6 = " . (4 * 6) . "\n";
+
+// Division of two numbers
+echo "8 / 2 = " . (8 / 2) . "\n";
+
+// Exponentiation of a number by itself
+echo "5 ** 3 = " . (5 ** 3) . "\n";
+
+// Concatenation of two strings
+echo "Hello, World!" . " " . "This is my PHP code." . "\n";
+
+// String manipulation
+$string = "My favorite fruit is apples.";
+echo $string . "\n"; // Outputs: My favorite fruit is apples.
