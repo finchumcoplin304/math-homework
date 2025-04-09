4892c129c8735e41b72e73b1ba347416e19d641a
finchumcoplin304 <finchumcoplin304@gmail.com>
2025-04-09 01:12:47 +0000

    Add 29.php

---
+++ src/29.php
@@ -0,0 +1,9 @@
+<?php
+// Example PHP code for Math homework assignment
+function exampleFunction() {
+    // This is an example function that calculates something.
+    $result = 5 + 3;
+    echo "The result of adding 5 and 3 is: " . $result;
+}
+exampleFunction();
+?>
